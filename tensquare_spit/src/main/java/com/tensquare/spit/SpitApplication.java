0cf042ef2a831fd0856d96c85782d03fcb7ba72d
wangshun <wangshun0529@163.com>
2019-11-23 17:11:58 +0800

    第一次提交

--- tensquare_spit/src/main/java/com/tensquare/spit/SpitApplication.java
+++ tensquare_spit/src/main/java/com/tensquare/spit/SpitApplication.java
@@ -2,13 +2,11 @@ package com.tensquare.spit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import util.IdWorker;
 import util.JwtUtil;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class SpitApplication {
 
     public static void main(String[] args) {
