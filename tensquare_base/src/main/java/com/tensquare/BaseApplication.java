fc0435608ad394804c0c2cb67e0d7c33175bc44f
wangshun <wangshun0529@163.com>
2019-11-23 14:35:46 +0800

    第一次提交

---
+++ tensquare_base/src/main/java/com/tensquare/BaseApplication.java
@@ -0,0 +1,23 @@
+package com.tensquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import util.IdWorker;
+
+/**
+ * @description: ${description}
+ * @Author wangshun
+ * @create: 2019-11-22 23:46
+ */
+@SpringBootApplication
+public class BaseApplication{
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class,args);
+    }
+
+    @Bean
+    public IdWorker idWorker(){
+        return new IdWorker(1,1);
+    }
+}
